28479208e7e2ecfd1acfa7ad55c76ac85900ac2d
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-07-10 22:13:25 -0400

    PCA wip wip

--- Erenchun-Matias-ProyectoEDA.R
+++ Erenchun-Matias-ProyectoEDA.R
@@ -6,27 +6,31 @@ library(ggplot2)
 #install.packages("factoextra")
 library("factoextra")
 
-
-url<-"https://drive.google.com/file/d/1G-ggtM7IRk013dj2-afEzegNczK1tEyj/view"
-
 #install.packages('gsheet')
 
 library(gsheet)
 
+url<-"https://drive.google.com/file/d/1G-ggtM7IRk013dj2-afEzegNczK1tEyj/view"
+
 data<-gsheet2tbl(url)
-View(data)
-data<-data[-1,]
-View(data)
-nameColums<-as.character(data[1, ])
-View(nameColums)
-head(data,n=10)
-names(data)<-nameColums
-View(data)
-dataF<-data[-1,]
-head(dataF,n=10)
-dataM<-dataF[-c(1),]
-head(dataM,n=10)
-# <- PCA(data,  graph = FALSE)
+#View(data)
+#------------------------------------------Preprocesar los datos para usar PCA----------------------------------------
+#los datos horiginales tienen doble cabecera ademas que son leidos como chr tenemso que,
+#corregir esto para poder usar PCA
+data<-data[-1,]#eliminamos la primera fila donde estan categorias como Social distancing
+#View(data)
+nameColums<-as.character(data[1, ])#obtenemos los nombres d elas columnas para posterios mente cambiarlos
+names(data)<-nameColums#cambiamos el nombre d elas columnas por los que ya tenemos (linea anterior)
+#View(data)
+dataF<-data[-1,]#eliminamos los nombre de los datos(ya estan como los nombres d elas columnas)
+paises<-as.vector(dataF$Country)#obtenemos los nombres de los paises para ponerlos como nombre de las filas
+dataF<-as.data.frame(dataF)
+dataF<-apply(dataF[,-1],2, as.integer)#pasamos los valores de chr a int en todas las columnas
+rownames(dataF)<-paises#agregamos los nombres de las filas
+summary(dataF)
+View(dataF)
+pcAmANITO<- PCA(dataF,  graph = FALSE)#aplicamos PCA
+View(pcAmANITO)
 
 
 
