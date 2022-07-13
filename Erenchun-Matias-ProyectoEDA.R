4a5fd4ad2df670edea44fb80447baafef997a6fd
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-07-13 15:27:32 -0400

    listo 6

--- Erenchun-Matias-ProyectoEDA.R
+++ Erenchun-Matias-ProyectoEDA.R
@@ -11,6 +11,9 @@ library("factoextra")
 library(gsheet)
 library(tidyverse)
 
+#install.packages("NbClust")
+library(NbClust)
+
 url<-"https://drive.google.com/file/d/1G-ggtM7IRk013dj2-afEzegNczK1tEyj/view"
 
 data<-gsheet2tbl(url)
@@ -47,8 +50,9 @@ summary(pcAmANITO)
 #para usar k-means https://stackoverflow.com/questions/61151538/compute-k-means-after-pca
 km<-kmeans(var$coord, centers = 4, nstart = 14)
 plot(pcAmANITO$ind$coord[,1:2],col=factor(km$cluster))
+
 km2<-kmeans(pcAmANITO$ind$coord, centers = 3, nstart = 14)
-#plot(pcAmANITO$ind$coord[,1:2],col=factor(km2$cluster))
+plot(pcAmANITO$ind$coord[,1:2],col=factor(km2$cluster))
 km2<-kmeans(pcAmANITO$ind$coord, centers = 5, nstart = 14)
 plot(pcAmANITO$ind$coord[,1:2],col=factor(km2$cluster))
 summary(km)
@@ -56,6 +60,12 @@ fviz_nbclust(pcAmANITO$ind$coord, kmeans, method = "gap_stat")
 fviz_nbclust(var$coord, kmeans, method = "gap_stat")
 
 fviz_nbclust(dataF, kmeans, method = "gap_stat")
+
+miCluster<-NbClust(dataF, diss=NULL, distance = "euclidean", min.nc=2, max.nc=10, method = "kmeans", index = "kl") 
+bClusters <- length(unique(miCluster$Best.partition))
+bClusters
+summary(bClusters)
+
 
 fviz_nbclust(var$coord, kmeans, method = "gap_stat")
 
@@ -86,3 +96,13 @@ fviz_dend(res.hc, palette = "jco",
           rect = TRUE, show_labels = TRUE)
 fviz_silhouette(res.hc)
 
+
+km.res <- kmeans(pcAmANITO$ind$coord, 3, nstart = 14)
+
+# 3. Visualize
+library("factoextra")
+fviz_cluster(km.res, data = pcAmANITO$ind$coord,
+             palette = c("#00AFBB","#2E9FDF", "#E7B800", "#FC4E07"),
+             ggtheme = theme_minimal(),
+             main = "Partitioning Clustering Plot"
+)
